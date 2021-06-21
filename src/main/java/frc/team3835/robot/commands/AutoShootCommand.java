598825a366cfa48c9c237a5ddc7309fe37b84665
GuruGuru19 <itaishiloh@gmail.com>
2021-06-21 23:45:22 +0300

    it was not the end :(
    we need to add auto commands and x-axis control on the auto shoot command

--- src/main/java/frc/team3835/robot/commands/AutoShootCommand.java
+++ src/main/java/frc/team3835/robot/commands/AutoShootCommand.java
@@ -5,6 +5,7 @@ import frc.team3835.robot.Constants;
 import frc.team3835.robot.MathAssistant;
 import frc.team3835.robot.UI;
 import frc.team3835.robot.subsystems.CameraSubsystem;
+import frc.team3835.robot.subsystems.DriveSubsystem;
 import frc.team3835.robot.subsystems.ShooterSubsystem;
 import frc.team3835.robot.subsystems.StorageSubsystem;
 
@@ -12,22 +13,28 @@ import frc.team3835.robot.subsystems.StorageSubsystem;
 public class AutoShootCommand extends CommandBase {
     private final ShooterSubsystem shooterSubsystem = ShooterSubsystem.getInstance();
     private final StorageSubsystem storageSubsystem = StorageSubsystem.getInstance();
+    private final DriveSubsystem driveSubsystem = DriveSubsystem.getInstance();
     private final CameraSubsystem cameraSubsystem = CameraSubsystem.getInstance();
 
     public AutoShootCommand() {
-        addRequirements(this.shooterSubsystem, this.storageSubsystem);
-
+        addRequirements(this.shooterSubsystem, this.storageSubsystem, this.driveSubsystem);
+        //TODO: set up x-axis control
     }
+
+
 
     @Override
     public void initialize() {
-        double distance = Math.tan(MathAssistant.degToRad(cameraSubsystem.getTy()+Constants.CAMERA_ANGLE))/(2.49555-Constants.CAMERA_HIGHT);
+        double distance = Math.tan(MathAssistant.degToRad(cameraSubsystem.getTy()+Constants.CAMERA_ANGLE))/(2.49555-Constants.CAMERA_HIGHT);//Y axis
         shooterSubsystem.setVelocityTarget(MathAssistant.ShootingAssistant.getShootingVelocity(distance));
         shooterSubsystem.setTargetAngle(MathAssistant.ShootingAssistant.getShootingAngle(distance));
+
+
     }
 
     @Override
     public void execute() {
+
         if (shooterSubsystem.isOnVelocitySetpoint() && shooterSubsystem.isOnAngleSetpoint()){
             storageSubsystem.setPower(Constants.STORAGE_POWER);
             withTimeout(Constants.SHOOTER_STORAGE_MOVING_TIME);
